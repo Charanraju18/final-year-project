175afb6731fc0b8d3d8766c07e5806b7109eee71
charan_raju_mb <charan.raju.36.4@mountblue.tech>
2026-04-01 22:08:12 +0530

    updated cors

--- server/server.js
+++ server/server.js
@@ -21,13 +21,9 @@ app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
-
-app.options("*", cors());
 
 app.get("/", (req, res) => res.send("Server is live..."));
 app.use("/api/users", userRouter);
