abad6bfaa499cc3a9594eabe7aabcc6d2c7f0883
charan_raju_mb <charan.raju.36.4@mountblue.tech>
2026-04-01 22:02:19 +0530

    fix cors preflight issue

--- server/server.js
+++ server/server.js
@@ -17,12 +17,17 @@ const PORT = process.env.PORT || 3000;
 await connectDb();
 
 app.use(express.json());
+
 app.use(
   cors({
     origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
+
+app.options("*", cors());
 
 app.get("/", (req, res) => res.send("Server is live..."));
 app.use("/api/users", userRouter);
